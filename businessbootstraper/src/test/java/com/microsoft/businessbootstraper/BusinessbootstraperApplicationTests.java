522b970ae30b16ef40b5fd1a3066cf0c6dca923e
Ajay Kumar Yadav <yadavajay109458@gmail.com>
2022-03-21 13:52:03 +0530

    test

---
+++ businessbootstraper/src/test/java/com/microsoft/businessbootstraper/BusinessbootstraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.businessbootstraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessbootstraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
