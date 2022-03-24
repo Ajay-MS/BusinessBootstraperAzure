3cbb89896d29a664a027ae22cc6e7fef37fbdbe1
Mayank agarwal <32328890+mayankagarwal98@users.noreply.github.com>
2022-03-24 16:25:30 +0530

    Add the apis

--- businessbootstraper/src/main/java/com/microsoft/businessbootstraper/models/PluginsMeta.java
+++ businessbootstraper/src/main/java/com/microsoft/businessbootstraper/models/PluginsMeta.java
@@ -1,52 +1,26 @@
-package com.example.com.FHL.Db;
+package com.microsoft.businessbootstraper.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Entity
-public class Pluginsmeta {
+@Data
+@ToString
+@NoArgsConstructor
+public class PluginsMeta {
 
     @javax.persistence.Id
     private long Id;
     private String Type;
     private String Data;
 
-    public Pluginsmeta(long id, String type, String data) {
+    public PluginsMeta(long id, String type, String data) {
         Id = id;
         Type = type;
         Data = data;
     }
 
-    public long getId() {
-        return Id;
-    }
-
-    public void setId(long id) {
-        Id = id;
-    }
-
-    public String getType() {
-        return Type;
-    }
-
-    public void setType(String type) {
-        Type = type;
-    }
-
-    public String getData() {
-        return Data;
-    }
-
-    public void setData(String data) {
-        Data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "Pluginsmeta{" +
-                "Id=" + Id +
-                ", Type='" + Type + '\'' +
-                ", Data='" + Data + '\'' +
-                '}';
-    }
 }
